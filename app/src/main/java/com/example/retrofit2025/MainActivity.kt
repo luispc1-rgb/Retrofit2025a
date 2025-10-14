6ac1a6a0a68e7c607f6574e3a005bee545027b14
luispc1-rgb <luis_pc1@tesch.edu.mx>
2025-10-14 16:30:38 -0600

    Update MainActivity.kt

--- app/src/main/java/com/example/retrofit2025/MainActivity.kt
+++ app/src/main/java/com/example/retrofit2025/MainActivity.kt
@@ -1,6 +1,6 @@
 // soy el equipo 4
 package com.example.retrofit2025
- 
+ //Soy Alejandro c:
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
